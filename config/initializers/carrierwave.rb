32ea2d860866602634b83af1bda2540e7144cfc9
Dan Pham <dan.p.pham97@gmail.com>
2019-11-06 20:25:26 +0000

    Lesson 39 Done

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     ENV["AKIARU4FWNE7CKNKY2IC"],        # required
-      aws_secret_access_key: ENV["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"],        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
     }
-    config.fog_directory  = ENV["dan-pham-nomster"]              # required
+    config.fog_directory  = ENV["AWS_BUCKET"]              # required
   end
